ddf54b53b6e8a769b430f363161d3f0b079e0afd
Sandiweb <118277281+Sandiwebsite01@users.noreply.github.com>
2024-03-06 12:51:43 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 
-global.owner = ["6282129006673"]
-global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
-global.pairingNumber = ""
+global.owner = ["6283893353148"]
+global.push_kontak_delay = 5000 //5 detik, 6000 = 6 detik
+global.pairingNumber = "6283893353148"
